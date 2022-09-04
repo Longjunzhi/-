28cf504c3f5f19e1ec75f59a12a0fbddfb1c5f31
Morijin <1036975003qq.com>
2022-09-04 17:04:22 +0800

    init

---
+++ java/cloudTravelShop/src/main/java/com/momo/cloudtravelshop/CloudTravelShopApplication.java
@@ -0,0 +1,13 @@
+package com.momo.cloudtravelshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTravelShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudTravelShopApplication.class, args);
+    }
+
+}
---
+++ java/cloudTravelShop/src/test/java/com/momo/cloudtravelshop/CloudTravelShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momo.cloudtravelshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTravelShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
